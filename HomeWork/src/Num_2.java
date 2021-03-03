9dd6c1bc240ae06dab61fbd8ab3154b8186127ae
Mitina Darina <mitinadari@gmail.com>
2021-03-03 15:48:23 +0200

    Mitina

---
+++ HomeWork/src/Num_2.java
@@ -0,0 +1,30 @@
+public class Num_2 {
+    public static void main(String[] args) {
+        String Day = "Tuesday";
+        switch (Day) {
+            case "Monday":
+                System.out.print("Cooking");
+                break;
+            case "Tuesday":
+                System.out.print("Singing");
+                break;
+            case "Wednesday":
+                System.out.print("Dancing");
+                break;
+            case "Thursday":
+                System.out.print("Time for a dream");
+                break;
+            case "Friday":
+                System.out.print("Dancing");
+                break;
+            case "Saturday":
+                System.out.print("Relaxation");
+                break;
+            case "Sunday":
+                System.out.print("Shopping");
+                break;
+            default:
+                System.out.println("mistake");
+        }
+    }
+}
---
+++ HomeWork/src/Num_5.java
@@ -0,0 +1,16 @@
+public class Num_5 {
+    public static void main(String[] args) {
+    double a = 19.7;
+    double b = 17.9;
+    int c = 10;
+    double m = Math.abs(c-a);
+    double n = Math.abs(c-b);
+    if (m==n){
+        System.out.println("Числа на равном растоянии от 10");
+    } else if (m>n) {
+        System.out.println("Число " + b + " ближе к числу " + c + " чем " + a);
+    } else if (n<m){
+        System.out.println("Число " + a + " ближе к числу " + c + " чем " + b);
+    }
+    }
+}
